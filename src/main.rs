506499cebdd5d8796f85cfa9e488d954239b7050
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2023-10-30 12:51:36 +0100

    feat: add basic mod config

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,21 @@ use bevy::prelude::*;
 
 type Effect = dyn Fn(&mut World) + Send + Sync;
 
-#[derive(Component)]
+#[derive(Clone, Component)]
 struct Mod {
     effect: Arc<Effect>,
 }
+
+type ModSlot = Option<Mod>;
+
+#[derive(Component)]
+struct ModConfig<const MOD_SLOTS: usize = 4, const ARCANE_SLOTS: usize = 0> {
+    special: ModSlot,
+    mods: [ModSlot; MOD_SLOTS],
+    arcanes: [ModSlot; ARCANE_SLOTS],
+}
+
+type RangedConfig = ModConfig<4, 1>;
 
 fn init_mods(mut commands: Commands) {
     commands.spawn(Mod {
@@ -29,7 +40,7 @@ fn auto_query(query: Query<&Mod>) {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(MinimalPlugins)
         .add_systems(Startup, init_mods)
         .add_systems(PostStartup, (manual_query, auto_query))
         .add_systems(Update, bevy::window::close_on_esc)
